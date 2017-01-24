6d348df6deca2c24c95f8c35105d3abd8a7e61a8
Matt Collins <mattcollins@Matts-MacBook-Pro.local>
2017-01-24 09:50:38 +0000

    Adding tags to constructor

--- src/Task.php
+++ src/Task.php
@@ -47,7 +47,8 @@ class Task extends Model
             ],
             'responsible_party_id'     => false,
             'attachments'              => false,
-            'pending_file_attachments' => false
+            'pending_file_attachments' => false,
+            'tags' => false
         ];
         $this->parent = 'todo-item';
         $this->action = 'todo_items';
